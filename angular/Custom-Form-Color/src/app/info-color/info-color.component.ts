f8cbd27e994aa756c61683dc12f508801bedcf4d
DuyDung <dinhvandung791@gmail.com>
2020-05-12 17:14:22 +0700

    create form infro color

---
+++ angular/Custom-Form-Color/src/app/info-color/info-color.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-color',
+  templateUrl: './info-color.component.html',
+  styleUrls: ['./info-color.component.css']
+})
+export class InfoColorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
